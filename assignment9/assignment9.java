c2c3617b2ea43779320d38c016301914a951d736
MidnightRaven12 <sparewriting2@gmail.com>
2025-03-17 08:07:58 -0400

    Update assignment9.java

--- assignment9/assignment9.java
+++ assignment9/assignment9.java
@@ -1,4 +1,13 @@
-class assignment9 {
+import java.lang.Math
 
+class assignment9 {
+	public static int[] merge(int[] array1, int[] array2, int descending) {
+		int[] array3 = new int[array1.length + array2.length];
+		int i, j, k;
+		while (k < array3.length) {
+			if (array1[i] < array2[j]) {
+				
+		}
+	}
 	public static void main(String[] args)
 }
